e6bf0c16f45d182d79d9fbf34960d07aa9202680
Tsuyoshi Yamamoto <mottsnite@gmail.com>
2010-07-01 19:43:48 +0900

    step2

--- src/test/groovy/com/mrhaki/blog/UserServiceSpecification.groovy
+++ src/test/groovy/com/mrhaki/blog/UserServiceSpecification.groovy
@@ -1,15 +1,23 @@
 package com.mrhaki.blog
 
 import spock.lang.Specification
+import spock.lang.Unroll
 
 class UserServiceSpecification extends Specification {
 
     def "Return total number of users"() {
-        setup: 'Create UserService instance with 2 users'
-        UserService userService = new UserService(users: ['mrhaki', 'hubert'])
+        setup: 'Create UserService instance with users'
+        UserService userService = new UserService(users: userList)
 
         expect: 'Invoke count() method'
-        2 == userService.count()
+        expectedCount == userService.count()
+
+        where:
+        expectedCount   | userList
+        0               | null
+        0               | []
+        1               | ['mrhaki']
+        2               | ['mrhaki', 'hubert']
     }
 
 }
